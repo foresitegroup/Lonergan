2a73b218b8ef8277cef324f2a292ebae92910265
foresitegroup <mark@foresitegrp.com>
2022-07-12 10:01:32 -0500

    Add new analytics

--- header.php
+++ header.php
@@ -11,19 +11,18 @@
   <link rel="icon" href="favicon.ico" type="image/x-icon">
   <script type="text/javascript" src="inc/swfobject.js"></script>
   
-  <!-- BEGIN Google Analytics -->
+  <!-- BEGIN Google Analytics - This will stop working July 1, 2023 -->
   <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
     ga('create', 'UA-31816102-1', 'auto');
     ga('send', 'pageview');
   </script>
   <!-- END Google Analytics -->
 
-  <!-- Global site tag (gtag.js) - Google Analytics (Greg) -->
+  <!-- Global site tag (gtag.js) - Google Analytics (Greg) - This will stop working July 1, 2023 -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-159342335-1"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
@@ -32,13 +31,22 @@
     gtag('config', 'UA-159342335-1');
   </script>
 
-  <!-- Google Tag Manager -->
+  <!-- Google Tag Manager - This will stop working July 1, 2023 -->
   <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
   })(window,document,'script','dataLayer','GTM-PMPLQZX');</script>
   <!-- End Google Tag Manager -->
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-CF9YMWH81W"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-CF9YMWH81W');
+  </script>
 </head>
 <body>
 
